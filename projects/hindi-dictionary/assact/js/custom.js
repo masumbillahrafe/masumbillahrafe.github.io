4bc902a990fa8d29c047bb542ca70a43068cb633
Masum Billah <118801745+masumbillahrafe@users.noreply.github.com>
2024-01-09 18:06:04 +0600

    custom.js

--- projects/hindi-dictionary/assact/js/custom.js
+++ projects/hindi-dictionary/assact/js/custom.js
@@ -7,6 +7,38 @@
 
 $(document).ready(function(){
   $(".popup-close").click(function(){
-    $("#full-popup").slideUp("slow");
+    $("#full-popup, .words").slideUp("slow");
   });
 });
+
+
+
+// words show
+
+$(document).ready(function(){
+  $("#auto_check, #popup_auto_check").click(function(){
+    $(".words, .popup-words").slideDown();
+  });
+});
+
+
+$(document).ready(function(){
+  $(".word").click(function(){
+    $(".words").slideUp();
+  });
+});
+
+
+
+// jason data
+var data = [];
+$.getJSON('F:/1.%20coding/Client%20Projects/Tasir%20Vai%20(Mirpur%20DOSH)/assact/js/jsondata.json',function(result){
+  $.each(result.entries, function(index, val){
+    data.push(val);
+  });
+});
+// console.log(data)
+
+      $( "#auto_check" ).autocomplete({
+        source: data
+      });
